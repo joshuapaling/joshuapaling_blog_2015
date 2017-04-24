71ec5ccef11ffe8935828e2804aea494222966ee
Joshua Paling <joshua.paling@gmail.com>
2017-04-24 10:12:39 +1000

    automated commit

--- config.rb
+++ config.rb
@@ -3,8 +3,8 @@
 ###
 
 # Time.zone = "UTC"
-page "/*", layout: 'layouts/default'
 page "/blog/*", layout: 'layouts/blog'
+page "/*", layout: 'layouts/default'
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
